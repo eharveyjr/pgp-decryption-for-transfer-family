782013316439a7447d2ef9b228175d1f9b87d734
EJ Harvey <eharvey@foxworldtravel.com>
2024-03-12 14:18:01 -0500

    Updated file to match with the Amazon Linux 3 standard.

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -3,14 +3,14 @@
 # Installing necessary python packages
 echo Installing required packages...
 cd /home/cloudshell-user
-sudo amazon-linux-extras enable python3.8
-sudo yum install python3.8 gcc make glibc-static bzip2 pip -y
+sudo dnf install python3.8 -y
+sudo dnf install python3.8 gcc make glibc-static bzip2 python3-pip -y
 echo Required packages installed. 
-pip3.8 install virtualenv
-virtualenv python
+pip3 install virtualenv
+python3.8 -m venv python
 cd python
-. bin/activate
-pip3.8 install python-gnupg
+source bin/activate
+pip3 install python-gnupg
 deactivate
 rm -rf ./bin
 mkdir ./python
@@ -19,9 +19,9 @@ mv lib ./python/
 # Downloading and building GPG binary from source
 wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-1.4.23.tar.bz2
 tar xjf gnupg-1.4.23.tar.bz2
-cd gnupg-1.4.23.tar.bz2
+cd gnupg-1.4.23
 /bin/bash /home/cloudshell-user/python/gnupg-1.4.23/configure
-make CLFAGS='-static'
+make CFLAGS='-static'
 cp g10/gpg /home/cloudshell-user/python/python
 cd /home/cloudshell-user/python/python
 chmod o+x gpg
